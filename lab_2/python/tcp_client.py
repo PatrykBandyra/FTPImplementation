bb46b735141ff3f002a134ba88227c35ef8244e4
Patryk Bandyra <patryk.bandyra@gmail.com>
2021-11-11 19:28:19 +0100

    Z 2.2 (w toku)

---
+++ lab_2/python/tcp_client.py
@@ -0,0 +1,18 @@
+import socket
+
+
+HOST = '127.0.0.1'
+PORT = 65000
+MESSAGE = 'Hello darkness, my old friend...'
+MESSAGE_NUM = 10
+
+
+if __name__ == '__main__':
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        message = MESSAGE.encode('ascii')
+        for i in range(MESSAGE_NUM):
+            s.sendall(message)
+            print(f'Message {i+1} sent')
+
+    print('Client closed')
---
+++ lab_2/python/tcp_server.py
@@ -0,0 +1,27 @@
+import socket
+
+
+HOST = '127.0.0.1'
+PORT = 65000
+BUF_SIZE = 16
+
+
+if __name__ == '__main__':
+    print(f'Will listen on {HOST}:{PORT}')
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen(5)
+
+        while True:
+            conn, address = s.accept()
+            with conn:
+                print(f'Connected from: {address}')
+                while True:
+                    data = conn.recv(BUF_SIZE)
+                    if not data:
+                        break
+                    print(f'Message from client {address}: {str(data.decode("ascii"))}')
+
+            conn.close()
+            print('Connection closed by client')
