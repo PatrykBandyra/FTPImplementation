cbf08e9470cb56e2b4bd232a1901f6ab42e68347
Patryk Bandyra <patryk.bandyra@gmail.com>
2021-12-31 20:31:35 +0100

    some progress

--- project/client.py
+++ project/client.py
@@ -2,18 +2,30 @@ import socket
 import threading
 import argparse
 import queue
-from typing import Tuple
+from typing import Tuple, Optional
 import pickle
+import hashlib
 
 
 class Client:
     """
     Multithread tcp client of simple FTP.
     """
+
+    HEADER_LENGTH = 10
+
     def __init__(self):
         args = Client.get_args()
         self.server_host = args.host
         self.server_port = args.port
+        self.mode = args.mode
+
+        # Thread-safe buffer for communicating between threads responsible for handling user input and sending commands
+        self.command_buffer = queue.Queue()
+
+        # Thread events
+        self.credentials_entered = threading.Event()
+        self.authenticated = threading.Event()
 
     @staticmethod
     def get_args() -> argparse.Namespace:
@@ -26,15 +38,145 @@ class Client:
                             help='Mode of establishing connection with server')
         return parser.parse_args()
 
+    @staticmethod
+    def send_object_message(s: socket.socket, message: object) -> None:
+        """
+        Serializes and sends 1 object message.
+        Sends header with message length.
+        """
+        message = pickle.dumps(message)
+        message_header = bytes(f'{len(message):<{Client.HEADER_LENGTH}}', 'utf-8')
+        s.sendall(message_header + message)
+
+    @staticmethod
+    def receive_object_message(s: socket.socket) -> Optional[dict]:
+        """
+        Receives 1 object message and deserialize it.
+        """
+        try:
+            message_header = s.recv(Client.HEADER_LENGTH)
+            if not message_header:
+                print('Failed to receive message header!')
+                return None
+
+            message_length = int(message_header.decode('utf-8').strip())
+            message = s.recv(message_length)
+            if not message:
+                print('Failed to receive message!')
+                return None
+
+            message = pickle.loads(message)
+            return message
+
+        except Exception as e:
+            print(f'Exception occurred during receiving a message!\n{e}')
+            return None
+
     def run(self) -> None:
 
         # TODO: tls wrapper
-        # 1. Establish connection with command channel
-        # 2. Authenticate
-        # 3. Agree on Data Channel
-        # 4. Start Data Channel Thread
-        # 5. Start Console Input Thread
-        # 6. Listen for new commands from user, verify and send them
+        # Establish connection with command channel
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(5)
+            try:
+                s.connect((self.server_host, self.server_port))
+            except socket.error as e:
+                print(f'Connection failed!\n{e}')
+
+            # Authenticate user
+            if not Client.authenticate_user(s):
+                print('Authentication failed!')
+                quit(1)
+
+            # Successful authentication - agree on Data Channel
+            if not self.agree_on_data_channel(s):
+                print('Could not agree on Data Channel!')
+                quit(1)
+
+            # Start Data Channel Thread
+            td = threading.Thread(target=self.handle_data_channel)
+            td.start()
+
+            # Start Console Input Thread
+            t = threading.Thread(target=self.handle_user_input)
+            t.start()
+
+            # Listen for new commands from user, verify and send them
+            self.handle_commands()
+
+    @staticmethod
+    def authenticate_user(s: socket.socket) -> bool:
+        """
+        Handles authentication of user with server.
+        """
+        username = input('Insert username: ').encode('utf-8')
+        password = input('Insert password: ').encode('utf-8')
+
+        hasher = hashlib.sha512()
+        hasher.update(password)
+        hashed_pass = hasher.hexdigest()
+
+        user_credentials = {'name': username, 'pass': hashed_pass}
+        Client.send_object_message(s, user_credentials)
+
+        status_message = Client.receive_object_message(s)
+        try:
+            if status_message['status'] == 'OK':
+                return True
+            return False
+
+        except Exception as e:
+            print(f'Exception occurred during authentication!\n{e}')
+            return False
+
+    def agree_on_data_channel(self, s: socket.socket):
+        """
+        Negotiates Data Channel
+        """
+        if self.mode == 'p':
+            self.connect_data_channel_passive(s)
+        else:
+            self.connect_data_channel_active(s)
+
+    def connect_data_channel_passive(self, s: socket.socket) -> Optional[socket.socket]:
+        """
+        Performs connection with server Data Channel in passive mode.
+        """
+        # Wait for server message to choose Data Channel connection mode
+        try:
+            message = Client.receive_object_message(s)
+            if not message['mode'] == 'ready':
+                return None
+
+            Client.send_object_message(s, {'mode': 'p'})
+            port_number_message = Client.receive_object_message(s)
+            port_number = int(port_number_message['port'])
+
+            # Connect to specified server port
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as data_s:
+                data_s.settimeout(5)
+                data_s.connect((self.server_host, port_number))
+
+
+        except Exception as e:
+            print(f'Exception occurred during attempt to establish connection with Data Channel in passive mode!\n{e}')
+            return None
+
+    def connect_data_channel_active(self, s: socket.socket):
+        """
+        Performs connection with server Data Channel in active mode.
+        """
+
+    def handle_data_channel(self):
+        pass
+
+    def handle_user_input(self) -> None:
+        """
+        Receives user commands from console, validates and put them in command buffer.
+        """
+        pass
+
+    def handle_commands(self):
         pass
 
 
--- project/server.py
+++ project/server.py
@@ -2,21 +2,26 @@ import socket
 import threading
 import argparse
 import queue
-from typing import Tuple
+from typing import Tuple, Optional
 import pickle
+import json
 
 
 class Server:
     """
     Multithread tcp socket server for managing simple FTP.
     """
+
+    HEADER_LENGTH = 10
+
     def __init__(self):
         args = Server.get_args()
         self.host = args.host
         self.port = args.port
 
         # Buffer for storing file paths of files currently being uploaded to the server
-        self.files_in_transfer_buffer = queue.Queue()  # Thread-safe
+        self.files_in_transfer_buffer = list()  # Not thread-safe -> critical section needed
+        self.files_in_transfer_mutex = threading.Lock()
 
     @staticmethod
     def get_args() -> argparse.Namespace:
@@ -26,6 +31,40 @@ class Server:
         parser.add_argument('-p', '--port', type=int, default=65000, metavar='',
                             help='Port number of Command Channel e.g. "65000"')
         return parser.parse_args()
+
+    @staticmethod
+    def send_object_message(s: socket.socket, message: object) -> None:
+        """
+        Serializes and sends 1 object message.
+        Sends header with message length.
+        """
+        message = pickle.dumps(message)
+        message_header = bytes(f'{len(message):<{Server.HEADER_LENGTH}}', 'utf-8')
+        s.sendall(message_header + message)
+
+    @staticmethod
+    def receive_object_message(s: socket.socket) -> Optional[dict]:
+        """
+        Receives 1 object message and deserialize it.
+        """
+        try:
+            message_header = s.recv(Server.HEADER_LENGTH)
+            if not message_header:
+                print('Failed to receive message header!')
+                return None
+
+            message_length = int(message_header.decode('utf-8').strip())
+            message = s.recv(message_length)
+            if not message:
+                print('Failed to receive message!')
+                return None
+
+            message = pickle.loads(message)
+            return message
+
+        except Exception as e:
+            print(f'Exception occurred during receiving a message!\n{e}')
+            return None
 
     def run(self) -> None:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
@@ -44,19 +83,71 @@ class Server:
         """
         Handles connection with individual client, manages Command Channel, starts thread handling Data Channel.
         """
-        while True:
-            data = conn.recv(1024)
-            if not data:  # Socket closed
-                break
-            # Logic
-            # 1. Authenticate user
-            # 2. Agree on Data Channel
-            # 3. Start Data Channel Thread
-            # 6. Listen for new commands from user, verify and respond to them
+        # Authenticate user
+        if not Server.authenticate_user(conn):
+            print(f'User authentication from {address} failed!')
+            return
+
+        # Agree on Data Channel
+        if not self.agree_on_data_channel(conn, address):
+            print(f'Failed to establish Data Channel connection with {address}')
+            return
+
+        # 3. Start Data Channel Thread
+        # 6. Listen for new commands from user, verify and respond to them
 
         print(f'Connection with {address} closed')
         conn.close()
 
+    @staticmethod
+    def authenticate_user(conn: socket.socket) -> bool:
+        user_credentials = Server.receive_object_message(conn)
+        if not user_credentials:
+            return False
+
+        try:
+            with open('auth.json', 'r', encoding='utf-8') as f:
+                auth_data = json.load(f)
+
+            # Compare hashes
+            if auth_data[user_credentials['name']] == user_credentials['pass']:
+                # Send "OK" status
+                Server.send_object_message(conn, {'status': 'OK'})
+                return True
+
+            return False
+
+        except Exception as e:
+            print(f'Exception occurred!\n{e}')
+            return False
+
+    def agree_on_data_channel(self, conn: socket.socket, address: Tuple[str, int]) -> Optional[socket.socket]:
+        """
+        Negotiates Data Channel
+        """
+        Server.send_object_message(conn, {'mode': 'ready'})
+        connection_mode_message = Server.receive_object_message(conn)
+        try:
+            if connection_mode_message['mode'] == 'p':
+                self.connect_data_channel_passive(conn, address)
+            elif connection_mode_message['mode'] == 'a':
+                self.connect_data_channel_active(conn, address)
+            else:
+                raise Exception('Client sent invalid Data Channel connection mode argument!')
+
+        except Exception as e:
+            print(f'Exception occurred during attempt to establish Data Channel connection with {address}\n{e}')
+            return None
+
+    def connect_data_channel_passive(self, conn: socket.socket, address: Tuple[str, int]):
+        """
+        Performs connection with server Data Channel in passive mode.
+        """
+
+    def connect_data_channel_active(self, s: socket.socket, address: Tuple[str, int]):
+        """
+        Performs connection with server Data Channel in active mode.
+        """
 
 def main() -> None:
     server = Server()
